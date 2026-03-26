54a9e9f5a3861afcecb21d4624e4431dec034c56
Lê Nguyễn Đức Đạt <3123411065@sv.sgu.edu.vn>
2026-03-26 13:39:03 +0700

    Revert "admin"
    This reverts commit 20809e7cd7859d893a0ebaef0479972041f32af1.

--- asset/js/admin/orders.js
+++
@@ -1,6 +0,0 @@
-    const filterCard = document.getElementById("filterCard");
-    const collapseBtn = document.getElementById("collapseBtn");
-
-    collapseBtn.addEventListener("click", () => {
-      filterCard.classList.toggle("collapsed");
-    });
